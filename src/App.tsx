a65ac4515a18fa7d251d9adc4c0f95a207b8a7a9
Vergil <vergil.huang@glegame.com>
2019-11-11 15:20:48 +0800

    验证

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,16 @@ const App: React.FC = () => {
       <NavigationHead onMenuClick={onOpenMenu} />
       <NavigationSideBar active={IsSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
       <main>
-        <div className="container black">
-          <div className="col-xs-12 yellow">1</div>
-          <div className="col-xs-12 col-sm-6 col-md-3 yellow">2</div>
-          <div className="col-xs-12 col-sm-6 col-md-3 yellow">3</div>
-          <div className="col-xs-12 col-sm-6 col-md-3 yellow">4</div>
-          <div className="col-xs-12 col-sm-6 col-md-3 yellow">5</div>
+        <div className="container black border">
+          <div className="col-xs-12 yellow border">1</div>
+          <div className="col-xs-12 col-sm-6 container border">
+            <div className="col-xs-12 col-sm-12 yellow border">2</div>
+            <div className="col-xs-12 col-sm-12 yellow border">3</div>
+          </div>
+          <div className="col-xs-12 col-sm-6 container border">
+            <div className="col-xs-12 col-sm-6 col-md-3 yellow border">4</div>
+            <div className="col-xs-12 col-sm-6 col-md-3 yellow border">5</div>
+          </div>
         </div>
       </main>
       <footer className="container gray">
