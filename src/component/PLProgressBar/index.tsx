cda23d631c623e679b19bd30fb008e68f2ef74cd
Vergil <zaq9716643@gmail.com>
2019-11-22 00:49:08 +0800

    create progress bar

---
+++ src/component/PLProgressBar/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import './style.sass'
+type Props = {
+
+}
+
+const ProgressBar: FC<Props> = () => {
+
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default ProgressBar
